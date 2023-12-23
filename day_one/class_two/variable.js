cfff4942067cf272fb47643924eadfb8984842fc
Chumki khatun <chumkikhatun370@gamil.com>
2023-12-23 17:05:59 +0600

    added class two

---
+++ day_one/class_two/variable.js
@@ -0,0 +1,11 @@
+var name = 'Chumki Khatun'
+var age = 20
+
+console.log(name);
+// output --> Chumki Khatun
+console.log(age);
+//  output --> 20
+console.log('My name is ' + name);
+// output --->My name is Chumki khatun
+console.log(' I am ' + age + ' year now ');
+//  output --> I am 20 year now
