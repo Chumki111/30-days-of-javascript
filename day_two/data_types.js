b732eaeaa89fc4cc477c31e2207d0dad1e24e5b9
Chumki khatun <chumkikhatun370@gamil.com>
2023-12-24 07:37:09 +0600

    added datatypes

---
+++ day_two/data_types.js
@@ -0,0 +1,22 @@
+// --------------------datatypes----------
+ 
+
+// -----------primitive---------
+Number
+23,234,3453,545,4
+
+String
+"dkjg" + 'kskjfd' +`kdjfghd`
+
+Boolean
+true,false
+
+NaN
+
+undefined 
+
+
+// ----------object-------
+Array
+Object
+Function
