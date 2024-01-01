e7ab47c7e43678885594e6a96d2996cc168e1dc6
Chumki khatun <chumkikhatun370@gamil.com>
2024-01-01 09:48:23 +0600

    added this

--- Modern_JavaScript/function.js
+++ Modern_JavaScript/function.js
@@ -7,20 +7,34 @@ function add(a,b){
 }
 
  let result = add(10,5)
- console.log(result);    //Answer : 15
+//  console.log(result);    //Answer : 15
 
 
  ///array function -> 
  let number =(a,b) =>{
     return a + b ;
 }    
-console.log(number(10,5));  // Answer : 15
+// console.log(number(10,5));  // Answer : 15
 
 let numbers = () => 10;
-console.log(numbers()) 
+// console.log(numbers()) 
 /// Answer : 10
 
 // let num = () => return 10;
 
-console.log(num()) 
+// console.log(num()) 
 //  SyntaxError: Unexpected token 'return'
+
+
+var JavaScript ={
+    name : 'JavaScript',
+    libraries : ['React' ,'Angular', 'Vue'],
+    printLibraries : function(){
+        // console.log(this); //{ name: 'JavaScript',libraries: [ 'React', 'Angular', 'Vue' ],printLibraries: [Function: printLibraries]}
+       
+        this.libraries.forEach((a) => console.log(`${this.name} loves ${a}`) /// JavaScript loves React,JavaScript loves Angular,JavaScript loves Vue
+        )
+    }
+}
+
+JavaScript.printLibraries();
